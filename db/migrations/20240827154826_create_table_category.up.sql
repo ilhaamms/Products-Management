b7287f09df1057c14938a11c89d03b0bbf5cd80d
Ilham Muhammad Sidiq <ilhaammuhammads@gmail.com>
2024-08-27 23:15:46 +0700

    add migrations

---
+++ db/migrations/20240827154826_create_table_category.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `categories` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
